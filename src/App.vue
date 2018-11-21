3b316a23fb7ff8aa96a3ee994098c3eb79eb20d7
王能 <1978511960@qq.com>
2018-11-21 15:30:14 +0800

    home

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <div id="app">
-    <x-header :left-options="{showBack: false}" left-options.preventGoBack="true" :title="title"><a slot="right">Feedback</a></x-header>
-    <router-view/>
-    <tabber-box></tabber-box>
+  <div id="app" style="height: 100%">
+    <view-box ref="viewBox" body-padding-top="2.3rem">
+      <x-header slot="header" :left-options="{showBack: false}" left-options.preventGoBack="true" :title="title" style="width:100%;position:absolute;left:0;top:0;z-index:100;" ><a slot="right">Feedback</a></x-header>
+      <router-view/>
+      <tabber-box></tabber-box>
+    </view-box>
   </div>
 </template>
 <script>
 import TabberBox from './components/Tabbar'
-import { XHeader } from 'vux'
+import { XHeader, ViewBox } from 'vux'
 export default {
   name: 'App',
   components: {
-    TabberBox, XHeader
+    TabberBox, XHeader, ViewBox
   },
   data () {
     return {
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,19 +1,68 @@
 <template>
   <div class="home">
-    <swiper class="swiper-box" :aspect-ratio="300/800" @on-index-change="bannerClick" v-model="banner_index">
-      <swiper-item class="swiper-demo-img" v-for="(item, index) in banner_list" :key="index">
-        <img :src="item.img">
+    <swiper class="swiper-box" :aspect-ratio="300/800" @on-index-change="bannerClick" v-model="banner_index" style="padding-bottom:0.5rem">
+      <swiper-item class="swiper-demo-img" v-for="(item, index) in banner_list" :key="index" style="padding-bottom:0.5rem">
+        <!--<img :src="item.img">-->
+        <x-img :src="item.img"></x-img>
       </swiper-item>
     </swiper>
+    <flexbox justify="space-between" class="title-box">
+      <flexbox-item><div class="left">领养机构</div></flexbox-item>
+      <flexbox-item><div class="right">共20家</div></flexbox-item>
+    </flexbox>
+    <scroller lock-y scrollbar-x class="lyjg-box">
+      <div class="list clear">
+        <div class="part" v-for="item in lyjg_list" :key="item.id">
+          <div class="img-box"><img :src="item.img" alt=""></div>
+          <div class="name">{{ item.name }}</div>
+          <div class="shop">{{ item.shop }}</div>
+          <div class="score">
+            <p class="num">待领养{{item.num}}只</p>
+            <div class="heart-box">
+              <img v-for="(sc,idx) in item.score" :key="idx" v-if="idx<item.score" src="@/assets/img/heart_full.png" alt="">
+              <img v-for="(sc,idx) in 5-item.score" :key="idx" v-if="idx<5-item.score" src="@/assets/img/heart_null.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </scroller>
+    <flexbox justify="space-between" class="title-box">
+      <flexbox-item><div class="left">寄养中心</div></flexbox-item>
+    </flexbox>
+    <flexbox justify="space-between" class="jyzx-box" :gutter="10">
+      <flexbox-item>
+        <div class="left">
+          <p>机构</p>
+          <p>INSTTTUTIONS</p>
+        </div>
+      </flexbox-item>
+      <flexbox-item>
+        <div class="right">
+          <p>个人</p>
+          <p>PERSONAL</p>
+        </div>
+      </flexbox-item>
+    </flexbox>
+    <flexbox justify="space-between" class="title-box">
+      <flexbox-item><div class="left">宠萌明星</div></flexbox-item>
+    </flexbox>
+    <div class="cmmx-box">
+      <flexbox justify="space-between" class="part" :gutter="10">
+        <flexbox-item>
+          <div></div>
+        </flexbox-item>
+        <flexbox-item><div class="right">共20家</div></flexbox-item>
+      </flexbox>
+    </div>
   </div>
 </template>
 
 <script>
-import { Swiper, SwiperItem } from 'vux'
+import { Swiper, SwiperItem, XImg, Cell, Flexbox, FlexboxItem, Scroller } from 'vux'
 export default {
   name: 'Home',
   components: {
-    Swiper, SwiperItem
+    Swiper, SwiperItem, XImg, Cell, Flexbox, FlexboxItem, Scroller
   },
   data () {
     return {
@@ -31,7 +80,42 @@ export default {
           url: 'javascript:'
         }
       ],
-      banner_index: 0
+      banner_index: 0,
+      lyjg_list: [
+        {
+          id: 1,
+          img: 'http://img1.imgtn.bdimg.com/it/u=163711717,1335413376&fm=200&gp=0.jpg',
+          name: '布拉吉',
+          shop: '爱助宠物之家',
+          num: '20',
+          score: 5
+        },
+        {
+          id: 2,
+          img: 'http://img1.imgtn.bdimg.com/it/u=3074798371,2768818906&fm=200&gp=0.jpg',
+          name: '布拉吉',
+          shop: '爱助宠物之家',
+          num: '20',
+          score: 4
+        },
+        {
+          id: 3,
+          img: 'http://img0.imgtn.bdimg.com/it/u=257643722,3894171946&fm=200&gp=0.jpg',
+          name: '布拉吉',
+          shop: '爱助宠物之家',
+          num: '20',
+          score: 5
+        },
+        {
+          id: 4,
+          img: 'http://img2.imgtn.bdimg.com/it/u=3964575684,7654796&fm=200&gp=0.jpg',
+          name: '布拉吉',
+          shop: '爱助宠物之家',
+          num: '20',
+          score: 3
+        }
+      ],
+      aa: 'dwdwdd'
     }
   },
   methods: {
